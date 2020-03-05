a5d785efe15e1d0901902f8eab27a28241ec5c9c
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-03-05 06:52:55 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,7 +6,7 @@ def run_guessing_game
     p "Goodbye!"
   elsif guess == '2'
     p "You guessed the correct number!"
-  elsif guess == 5
+  elsif num == 5
       p "Sorry! The computer guessed 6."
   end
 
