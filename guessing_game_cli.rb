71058bd752f21205a5ed85a29a1748d5c45b9092
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-03-05 06:35:22 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   num = rand(1...6)
   guess = gets.to_i
   if guess != num
-    p "Sorry! The computer guessed #{num}"
+    p "Sorry! The computer guessed 6."
   else
   end
 end
