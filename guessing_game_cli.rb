f948491af23bac0b294979391afdcd804652ae99
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-03-05 06:08:42 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,7 +2,7 @@ def run_guessing_game
   num = rand(1..6)
   puts "Guess a number"
   your_num = gets.chomp
-  if your_num == exit
+  if your_num == 'exit'
     p "Goodbye!"
   else
     your_num.to_i
