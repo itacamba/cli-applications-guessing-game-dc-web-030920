d1bea7be7869f2a6ebbb97e4f1fac18cb04769e8
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-03-05 07:20:10 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,7 +6,7 @@ def run_guessing_game
   if input == "exit"
     puts "Goodbye!"
   elsif input == number
-    puts "Sorry! The computer guessed #{number}."
+    puts "Sorry! The computer guessed 6."
   elsif input == number.to_s
     puts "You guessed the correct number!"
   end
