58621e8a7e38f73c31d159741bb3cb7a7c82ce3c
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-03-05 06:04:39 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 def run_guessing_game
   num = rand(1..6)
   puts "Guess a number"
-  your_num = gets.chomp.
+  your_num = gets.chomp
   if your_num.to_i == num
     puts "You guessed the correct number!"
   elsif your_num.to_i != num
